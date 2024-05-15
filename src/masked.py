02b941a848850cd8648c41dff136579f924a3310
Pavel Isaev <killroy1297@gmail.com>
2024-05-16 02:05:45 +0300

    final

--- src/masked.py
+++ src/masked.py
@@ -1,11 +1,12 @@
 def mask_card_number(card_number):
-    if card_number:
-        return f"{card_number[:17]} {'X' * 4}"
-    else:
-        return None
+    card_name = card_number.split(' ')[0]
+    card_number = ''.join(filter(str.isdigit, card_number))[:6]
+    visible_part = f"{card_name} {' '.join([card_number[:4], 'XX**', '****', card_number[-4:]])}"
+    return visible_part
+
+
+
 
 def mask_account_number(account_number):
-    if account_number:
-        return account_number[-4:]
-    else:
-        return None
+    masked_number = '**' + account_number[-4:]
+    return masked_number
--- src/transactions.py
+++ src/transactions.py
@@ -1,66 +1,41 @@
 import os
-from masked import mask_card_number, mask_account_number
 import json
 from datetime import datetime
-
-
-def print_last_transactions():
-    """
-    Выводит на экран последние 5 выполненных операций.
-
-    Операции разделяются пустой строкой.
-    Дата перевода представлена в формате ДД.ММ.ГГГГ.
-    Номер карты замаскирован и не отображается целиком.
-    Номер счета замаскирован и не отображается целиком.
-    """
-    # Определение пути к файлу operations.json
-    current_directory = os.path.dirname(os.path.abspath(__file__))  # Получаем текущую директорию
-    file_path = os.path.join(current_directory, '..', 'data', 'operations.json')  # Объединяем путь к файлу
-
-    # Открытие файла с данными операций
-    with open(file_path, 'r', encoding='utf-8') as file:
-        operations = json.load(file)
-
-    # Фильтрация и сортировка выполненных операций по дате (от новых к старым)
-    executed_operations = sorted(
-        (op for op in operations if op.get('state') == 'EXECUTED'),
-        key=lambda x: datetime.strptime(x['date'], '%Y-%m-%dT%H:%M:%S.%f'),
-        reverse=True
-    )
-
-    # Получение последних 5 выполненных операций (самые новые)
-    last_executed_operations = executed_operations[:5]
-
-    # Вывод информации о количестве операций
-    print("Сверху списка находятся самые последние операции (по дате):")
-
-    for operation in last_executed_operations:
-        print_transaction(operation)
+from masked import mask_card_number, mask_account_number
 
 
 def print_transaction(operation):
     date = datetime.strptime(operation['date'], '%Y-%m-%dT%H:%M:%S.%f').strftime('%d.%m.%Y')
     description = operation['description']
-    from_account = operation.get('from', '')
+    from_account = operation.get('from', 'Нет данных') or 'Нет данных'
     to_account = operation['to']
-    operation_amount = operation['operationAmount']
-    amount = operation_amount['amount']
-    currency = operation_amount['currency']['name']
+    amount = operation['operationAmount']['amount']
+    currency = operation['operationAmount']['currency']['name']
 
-    # Маскируем номер карты
-    masked_from_account = mask_card_number(from_account) if from_account else "Нет данных"
+    if 'from' in operation:
+        from_account = mask_card_number(from_account) if from_account.startswith('Maestro') or from_account.startswith(
+            'Visa') else mask_account_number(from_account)
 
-    # Маскируем номер счета
-    masked_to_account = mask_account_number(to_account) if to_account else "Нет данных"
+    to_account = mask_account_number(to_account)
 
-    if masked_from_account:
-        masked_from_account = ' '.join([masked_from_account[:17], 'XX**', '****'])
-
-    if masked_to_account:
-        masked_to_account = 'Счет **' + masked_to_account[-4:]
-
-    # Выводим операцию
     print(f"{date} {description}")
-    print(f"{masked_from_account} -> {masked_to_account}")
-    print(f"{amount} {currency}")
-    print()
+    print(f"{from_account} -> Счет {to_account}")
+    print(f"{amount} {currency}\n")
+
+
+def print_last_transactions():
+    file_path = "C:\\Users\\killr\\PycharmProjects\\kursach_2\\data\\operations.json"
+    if not os.path.exists(file_path):
+        print(f"File not found: {file_path}")
+        return
+
+    with open(file_path, 'r', encoding='utf-8') as file:
+        operations = json.load(file)
+    executed_operations = [op for op in operations if op.get('state') == 'EXECUTED']
+    sorted_operations = sorted(executed_operations, key=lambda x: datetime.strptime(x['date'], '%Y-%m-%dT%H:%M:%S.%f'),
+                               reverse=True)
+    last_5_operations = sorted_operations[:5]
+
+    print("Сверху списка находятся самые последние операции (по дате):")
+    for operation in last_5_operations:
+        print_transaction(operation)
