eaa8d0e35684978a8e579e0587cbecd3de3a75de
Pavel Isaev <killroy1297@gmail.com>
2024-05-23 20:48:02 +0300

    fine2

--- src/transactions.py
+++ src/transactions.py
@@ -1,32 +1,41 @@
-import json
 import os
+import json
+from datetime import datetime
 from masked import mask_card_number, mask_account_number
 
 
+def print_transaction(operation):
+    date = datetime.strptime(operation['date'], '%Y-%m-%dT%H:%M:%S.%f').strftime('%d.%m.%Y')
+    description = operation['description']
+    from_account = operation.get('from', 'Нет данных') or 'Нет данных'
+    to_account = operation['to']
+    amount = operation['operationAmount']['amount']
+    currency = operation['operationAmount']['currency']['name']
+
+    if 'from' in operation:
+        from_account = mask_card_number(from_account) if from_account.startswith('Maestro') or from_account.startswith(
+            'Visa') else mask_account_number(from_account)
+
+    to_account = mask_account_number(to_account)
+
+    print(f"{date} {description}")
+    print(f"{from_account} -> Счет {to_account}")
+    print(f"{amount} {currency}\n")
+
+
 def print_last_transactions():
-    with open('../data/operations.json', 'r', encoding='utf-8') as file:
+    file_path = "C:\\Users\\killr\\PycharmProjects\\kursach_2\\data\\operations.json"
+    if not os.path.exists(file_path):
+        print(f"File not found: {file_path}")
+        return
+
+    with open(file_path, 'r', encoding='utf-8') as file:
         operations = json.load(file)
-
     executed_operations = [op for op in operations if op.get('state') == 'EXECUTED']
-    last_executed_operations = sorted(executed_operations, key=lambda x: x['date'], reverse=True)[:5]
+    sorted_operations = sorted(executed_operations, key=lambda x: datetime.strptime(x['date'], '%Y-%m-%dT%H:%M:%S.%f'),
+                               reverse=True)
+    last_5_operations = sorted_operations[:5]
 
-    for op in last_executed_operations:
-        date = op['date']
-        description = op['description']
-        from_account = op.get('from', '')
-        to_account = op.get('to', '')
-        operation_amount = op.get('operationAmount', '')
-        if isinstance(operation_amount, str):
-            amount, currency = operation_amount.split(' ')
-        else:
-            amount = currency = ''
-        masked_from_account = mask_card_number(from_account)
-        masked_to_account = mask_account_number(to_account)
-
-        print(f"{date} {description}")
-        print(f"{masked_from_account} -> {masked_to_account}")
-        print(f"{amount} {currency}")
-        print()
-
-
-print("Current directory:", os.getcwd())
+    print("Сверху списка находятся самые последние операции (по дате):")
+    for operation in last_5_operations:
+        print_transaction(operation)
---
+++ test/test_transactions.py
@@ -0,0 +1,8 @@
+import pytest
+from src.transactions import print_last_transactions
+
+def test_print_last_transactions(capsys):
+    print_last_transactions()
+    captured = capsys.readouterr()
+    assert "Перевод организации" in captured.out
+    assert "Открытие вклада" in captured.out
