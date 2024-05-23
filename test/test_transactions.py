6a4f4ded852ef4ccd688e528ce79b7bc8fde7d9e
Pavel Isaev <killroy1297@gmail.com>
2024-05-23 23:59:30 +0300

    fine2

--- test/test_transactions.py
+++ test/test_transactions.py
@@ -5,4 +5,3 @@ def test_print_last_transactions(capsys):
     print_last_transactions()
     captured = capsys.readouterr()
     assert "Перевод организации" in captured.out
-    assert "Открытие вклада" in captured.out
