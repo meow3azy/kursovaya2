9520890a11f28dfdb77f9d26c0aaba3d3b7bfc84
Pavel Isaev <killroy1297@gmail.com>
2024-06-14 03:35:40 +0300

    1

--- src/masked.py
+++ src/masked.py
@@ -1,16 +1,7 @@
 def mask_card_number(card_number):
-    card_parts = card_number.split()
-    card_name = ' '.join(card_parts[:-1])
-    card_digits = card_parts[-1]
-
-    masked_digits = f"{card_digits[:4]} XX** **** {card_digits[-4:]}"
-    return f"{card_name} {masked_digits}"
-def mask_card_number(card_number):
-    if len(card_number) > 16:
-        visible_part = f"{card_number[:4]} {card_number[4:6]}** **** {card_number[-4:]}"
-    else:
-        visible_part = f"{card_number[:4]} XX** **** {card_number[-4:]}"
-    return visible_part
+    card_name, card_number = card_number.rsplit(' ', 1)
+    masked_number = f"{card_number[:4]} {card_number[4:6]}XX **** {card_number[-4:]}"
+    return f"{card_name} {masked_number}"
 
 def mask_account_number(account_number):
     return f"**{account_number[-4:]}"
